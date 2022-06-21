5f13a636adc5c7eb39a5c0f14f59fb06bed6f9ac
Hugo <cozichugo95@gmail.com>
2022-06-21 23:21:48 +0200

    Adding the color change on hover on divs

--- script.js
+++ script.js
@@ -1,30 +1,45 @@
 const wrapper = document.querySelector("body > div.wrapper");
 const btns = document.querySelectorAll('.choice-btns > button');
 
-const nbGrid = btns.forEach(btn => {
+//Selection of the grid format
+const gridFormat = btns.forEach(btn => {
     const nb = parseInt(btn.textContent);
     btn.addEventListener('click', () => setGrid(nb));
 });
 
 
+//Create the starting grid
 function createGrid () {
     const newGrid = document.createElement('div');
     newGrid.setAttribute('class', 'grid');
     wrapper.appendChild(newGrid);
-
 };
 
-function setGrid (nb) {
+
+//Change the format of the grid
+function setGrid(nb) {
     const grid = document.querySelector("body > div.wrapper > div.grid");
     while (grid.hasChildNodes()) {
         grid.removeChild(grid.firstChild);
     }
     grid.style = `width: 50vw; height: 80vh;margin: 0 auto; display: grid; grid-template-columns: repeat(${nb}, 1fr); grid-template-rows: repeat(${nb}, 1fr)`;
-    for(i=0; i<nb*nb; i++) {
+    for (i = 0; i < nb * nb; i++) {
         const div = document.createElement('div');
         div.setAttribute('class', i);
         grid.appendChild(div);
     }
+    
+    //Select pixels to add colors
+    const pixels = document.querySelectorAll('.grid > div').forEach(pixel => {
+        pixel.addEventListener('mouseover', () => addColor(pixel))
+    });
+}
+
+
+//Add the colors to the pixels
+function addColor(pixel, color) {
+    color = "red"
+    pixel.style.backgroundColor = color;
 }
 
 createGrid();
